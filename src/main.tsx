7376d5b651ea8b5bad34d433f21800689f78cab0
CaioMiranda12 <caioclm04@gmail.com>
2026-05-05 20:53:11 -0300

    feat: add router provider + toast on main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -1,13 +1,14 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+import { RouterProvider } from 'react-router-dom'
+import { router } from './router'
 import './index.css'
-import Home from './pages/Home.tsx'
-import { BrowserRouter } from 'react-router-dom'
+import 'react-toastify/dist/ReactToastify.css'
+import { ToastContainer } from 'react-toastify'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <BrowserRouter>
-      <Home />
-    </BrowserRouter>
-  </StrictMode>,
+    <ToastContainer position="bottom-right" autoClose={4000} />
+    <RouterProvider router={router} />
+  </StrictMode>
 )
